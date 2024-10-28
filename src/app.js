6add5f12d325e4872e16315002a4b2ec3e559e2a
Ainesh Sinha <studentainesh@gmail.com>
2024-10-28 16:03:44 +0530

    Added app configs using express functions

--- src/app.js
+++ src/app.js
@@ -9,8 +9,20 @@ app.use(cors({
     credentials: true,
 }))
 
+
+/**
+ * Configures the Express application to parse incoming JSON requests with a maximum size of 16kb.
+ */
 app.use(express.json({limit: '16kb'}))
 
+/**
+ * Configures the Express application to parse incoming URL-encoded requests with a maximum size of 16kb.
+ */
+app.use(express.urlencoded({extended: true, limit: '16kb'}))
+
+/**
+ * Serves static files from the 'public' directory.
+ */
 app.use(express.static('public'))
 
 app.use(cookieParser())
